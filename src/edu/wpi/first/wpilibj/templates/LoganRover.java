59cb1ac511ac421b464d364beeff79fcf5e6e532
Tanner Smith <tanner@tssoftware.net>
2009-12-12 15:49:47 -0500

    Add basic drivetrain support

--- src/edu/wpi/first/wpilibj/templates/LoganRover.java
+++ src/edu/wpi/first/wpilibj/templates/LoganRover.java
@@ -9,6 +9,8 @@ package edu.wpi.first.wpilibj.templates;
 
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -18,26 +20,32 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  * directory.
  */
 public class LoganRover extends IterativeRobot {
+    private RobotDrive drivetrain;
+    private Joystick joystickOne;
+
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     public void robotInit() {
+        joystickOne = new Joystick(1);
 
+        drivetrain = new RobotDrive(3, 1);
+        drivetrain.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
     }
 
     /**
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
-
+        drivetrain.arcadeDrive(0, 0);
     }
 
     /**
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-        
+        drivetrain.arcadeDrive(joystickOne);
     }
     
 }
